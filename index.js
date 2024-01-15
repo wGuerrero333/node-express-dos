33f84ef51f2bdd8e09854f33e65fb740a9f306b4
wGuerrero333 <wguerreroc1969@gmail.com>
2024-01-15 13:07:51 -0500

    Se ignora archivo .env y  el ssl en index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from 'express'
-
+// permite cargar varibles de entorno para pruebas en development en produccion no es necesario
+import {config } from 'dotenv' 
+// pg es un modulo para conectarse a PostgreSQL
+import pg from 'pg'
+config()
 const app = express()
-
+const conexion = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    // solo necesario en DEVELOPMENT
+    // ssl:true
+})
 // rutas
 app.get('/',(req,res)=>{
-    res.send("Respuesta texto")
+    res.send("Respuesta send texto ")
+})
+
+app.get('/conexion', async(req,res)=>{
+    const resultado =  await conexion.query('SELECT NOW()')
+    return res.json(resultado.rows[0])
 })
 
 app.listen( 3000)
